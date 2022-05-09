af1388bea29525887110fcc490d54359344a09ee
JSebastian2002 <jspenarreta@utpl.edu.ec>
2022-05-09 11:29:44 -0500

    -aClase05

--- EjemplosRelacionesClases/EjemplosRelacionesDos/src/paquete2/Ciudad.java
+++ EjemplosRelacionesClases/EjemplosRelacionesDos/src/paquete2/Ciudad.java
@@ -1,27 +1,56 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete2;
-
 /**
  *
- * @author reroes
+ * @author SALA I
  */
-public class Ciudad {
-    
-    private String nombre;
-    
-    public Ciudad(String n){
-        nombre = n;
+public class Ejecutor02 {
+    public static void main(String[] args) {
+        //1.crear una persona de barrio el Valle de la ciudad de Loja
+        //2.crear una persona derl bariio central de la ciudad de Cuenca
+        //3.crear una persona del bariio centenario de la ciudad de Guayaquil
+       
+        Ciudad ciudad1 = new Ciudad("Loja");
+        Ciudad ciudad2 = new Ciudad("Cuenca");
+        Ciudad ciudad3 = new Ciudad("Guayaquil");
+        
+        Barrio barrio1 = new Barrio("el Valle", ciudad1);
+        Barrio barrio2 = new Barrio("Central", ciudad2);
+        Barrio barrio3 = new Barrio("Centenario", ciudad3);
+        
+        Persona persona1 = new Persona("Alvaro Lozano", "110459587212", barrio1);
+        Persona persona2 = new Persona("Diego Peñarreta", "110249487431", barrio2);
+        Persona persona3 = new Persona("Jose Cueva", "110250598371", barrio3);
+        
+        
+        System.out.println("------------------------------------------");
+        System.out.printf("El nombre de la persona: \n%s El barrio de "
+                + "la persona es: \n%s\n Y pertenece a la "
+                + "ciudad: %s\n",
+                persona1.obtenerNombre(),
+                persona1.obtenerBarrio().obtenerNombre(),
+                persona1.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        System.out.println("------------------------------------------");
+        
+        System.out.println("------------------------------------------");
+        System.out.printf("El nombre de la persona: %s El barrio de la persona es: %s\n Y pertenece a la "
+                + "ciudad: %s\n",
+                persona2.obtenerNombre(),
+                persona2.obtenerBarrio().obtenerNombre(),
+                persona2.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        System.out.println("------------------------------------------");
+        
+        System.out.println("------------------------------------------");
+        System.out.printf("El nombre de la persona: %s El barrio de la persona es: %s\n Y pertenece a la "
+                + "ciudad: %s\n",
+                persona3.obtenerNombre(),
+                persona3.obtenerBarrio().obtenerNombre(),
+                persona3.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        System.out.println("------------------------------------------");
+        
     }
     
-    public void establecerNombre(String n){
-        nombre = n;
-    }
-    
-    public String obtenerNombre(){
-        return nombre;
-    }
 }
