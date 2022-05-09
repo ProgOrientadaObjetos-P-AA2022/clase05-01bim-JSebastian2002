9cc37f7b0562b37a007480ab403ff70b542a18ed
JSebastian2002 <kevinbust0232@gmail.com>
2022-05-09 11:23:38 -0500

    Clase 05

---
+++ EjemplosRelacionesClases/EjemploRelaciones/src/paquete01/Ejecutor03.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejecutor03 {
+
+    public static void main(String[] args) {
+        String procesador = "Dell";
+        UnidadDVD ft = new UnidadDVD("Rogue");
+        Computadora computadora = new Computadora(procesador,  ft);
+        ft.establecerCosto(450);
+        
+        System.out.printf("Computadora:\nProcesador: %s\n"
+                + "Unidad de DVD: %s\n"
+                + "Costo de Unidad DVD: %.2f\n",
+                computadora.obtenerTipoProcesador(),
+                computadora.obtenerUnidadDvd().obtenerMarca(),
+                computadora.obtenerUnidadDvd().obtenerCosto());
+        
+    }
+
+}
